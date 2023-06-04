6dfe4f00fdb0ccdc0a80a5840da4d7e187418734
FacuMeoni <meonifacu@gmail.com>
2023-06-04 11:25:07 -0300

    Models listos

--- api/src/db.js
+++ api/src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASS, DB_HOST, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -28,14 +28,13 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+// Destructuring models para relacionar los modelos.
+const { Dog, Temperament } = sequelize.models;
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+Dog.belongsToMany(Temperament, { through : 'dogsTemperaments'})
+Temperament.belongsToMany(Dog, { through : 'dogsTemperaments'})
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // exportamos los modelos
+  conn: sequelize,     // exportamos la conexion
 };
---
+++ api/src/models/User.js
@@ -0,0 +1,30 @@
+const { DataTypes } =  require('sequelize')
+
+module.exports =  async(sequelize) => {
+    sequelize.define('User', {
+        id:{
+            type:DataTypes.UUID,
+            primaryKey:true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        username:{
+            type:DataTypes.STRING(20),
+            allowNull:false,
+            unique: true
+        },
+        email:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                isEmail:true
+            }
+        },
+        password:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                is: /\d/
+              }
+        }
+    })
+}
