c6bfad0a0a8d0f871a51a4abfe4f66fcc567e027
FacuMeoni <meonifacu@gmail.com>
2023-06-23 21:31:56 -0300

    aprobado :)

--- client/src/components/Paginate/Paginate.jsx
+++ client/src/components/Paginate/Paginate.jsx
@@ -5,9 +5,11 @@ const Paginate =({currentPage, totalPages, paginate}) => {
 
   return (
     <div className={css.allPage_container}>
+     <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}> Prev </button>
       {totalPages && totalPages.map(page => (
         <button  key={page}onClick={() => paginate(page)} className={currentPage === page ? css.pageCurrent : css.page}>{page}</button>
       ))}
+      <button onClick={() => paginate(currentPage + 1)} disabled={currentPage === 22}> Next </button>
     </div>
   );
 }
